64fb26a804d2d4918b8e0216ff3ef82315519372
juakuz <shoaking@gmail.com>
2016-11-14 19:09:09 -0300

    Add files via upload

---
+++ jsEntradaSalida-5.js
@@ -0,0 +1,13 @@
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function Mostar()
+{	
+	var mitext;
+	mitexto=prompt("Su nombre es");
+	document.getElementById("elNombre"). value=mitexto;
+
+	var mitext;
+	mitexto=prompt("y su edad es de");
+	document.getElementById("laEdad"). value=mitexto;
+}
+
